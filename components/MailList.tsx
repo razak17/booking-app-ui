a4367ae22787bd75e209aab039a5cf28a18cac04
Razak <moabdulrazak17@gmail.com>
2022-09-19 18:30:09 +0000

    feat(components): footer design

--- components/MailList.tsx
+++ components/MailList.tsx
@@ -4,7 +4,7 @@ function MailList() {
 	return (
 		<div className={styles.mail}>
 			<h1 className={styles.mailTitle}>Save time, save money!</h1>
-			<span className={styles.mailDesc}>Sign up and we'll send the best deals to you</span>
+			<span className={styles.mailDesc}>Sign up and we&apos;ll send the best deals to you</span>
 			<div className={styles.mailInputContainer}>
 				<input type='text' placeholder='Your Email' />
 				<button>Subscribe</button>
