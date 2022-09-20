9049363b8d66e0994a229f0d3d22b65d8722425f
Razak <moabdulrazak17@gmail.com>
2022-09-20 19:38:14 +0000

    chore(components): loader component

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Loader() {
 	return (
-		<div style={{ display: 'flex' }}>
+		<div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
 			<p>Loading...</p>
 		</div>
 	);
